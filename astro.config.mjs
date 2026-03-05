a43c2b012760923158f856197634cdfca84b0c32
Sean Hwang <bigwater@snu.ac.kr>
2026-03-05 23:51:03 +0900

    First commit!

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://bigwater7655.github.io',
+    base: '/dlblog-astro'
+});
