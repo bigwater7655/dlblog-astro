286e59c7b95f40a0844938d6b6c26a6975d7804b
Sean Hwang <bigwater@snu.ac.kr>
2026-03-06 23:33:10 +0900

    Fixed base URL in configs to include trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bigwater7655.github.io',
-  base: '/dlblog-astro',
+  base: '/dlblog-astro/',
   integrations: [mdx()],
   vite: {
     resolve: {
